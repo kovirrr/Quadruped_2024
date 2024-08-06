175648065be4236635ee9aa8716653d4e354cb50
Kovi Ressler <koviressler@gmail.com>
2024-08-06 12:49:24 -0400

    made main file and added other files

---
+++ main.py
@@ -0,0 +1,21 @@
+#this code will have everything tied together
+
+#imports
+
+#classes
+class Robot():
+  def __init__(self, speed):
+    self.speed = speed
+  def __repr__():
+    return ""
+
+  def control():
+    pass
+
+#functions
+def main():
+  pass
+
+#main code
+if __name__ == "__main__":
+    main()
